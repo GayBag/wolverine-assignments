4b78943e0ebbad7eb3118941c9020c8c3e586b51
GayBag <111243339+GayBag@users.noreply.github.com>
2022-10-02 17:53:48 +0400

    Add files via upload

---
+++ sep28/replace.cpp
@@ -0,0 +1,51 @@
+# include <iostream>
+# include <string>
+
+std::string replaceAll(std::string &,const std::string  &t, const std::string &r);
+
+
+int main ()
+{
+	std::string s = "hello,world";
+	std::string t = "l";
+	std::string r = "ch";
+
+
+	std::cout << replaceAll(s, t, r) << std::endl;
+
+	return 0;
+}
+
+std::string replaceAll(std::string &s,  const std::string &t,  const std::string & r)
+
+{    
+		int pos = 0;
+		int ind = 0;
+		std::string ss = s;
+               
+		
+	while ((pos = ss.find(t)) != std::string::npos)
+	{
+          	     
+		    pos = ss.find(t);
+                  
+		   ind += (s.substr(0,pos)).size();
+                       
+                    
+		    s.erase(ind,t.size());
+                     
+		    s.insert(ind,r);
+		    
+		    ind += r.size(); 
+		  
+		    
+		   ss.erase(0, pos+t.size());
+	       
+	          
+	}
+
+
+	
+
+	return s;
+}
